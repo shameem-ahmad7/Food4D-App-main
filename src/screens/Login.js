1e97c0a4918fd968b1cb13b151d639ea0d190477
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-01-31 18:28:42 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://food4-u-mern-project-ot2d.vercel.app/api/loginuser", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
