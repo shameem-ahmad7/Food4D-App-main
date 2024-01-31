53c98088a1f8364180a3e449ea1f8a38333a8a23
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-01-31 18:29:35 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => { 
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("https://food4-u-mern-project-ot2d.vercel.app/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
